9eb41e475d9c3a10a6bb1c114047d4fd373b49f0
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 16:50:26 +0800

    Investigating3

--- buildclr.sh
+++ buildclr.sh
@@ -1,6 +1,6 @@
 cd $GITHUB_WORKSPACE/runtime
 export ROOTFS_DIR=$GITHUB_WORKSPACE/runtime/.tools/android-rootfs/android-ndk-r21/sysroot
-tree  $ROOTFS_DIR
+cp $ROOTFS_DIR/android_platform $ROOTFS_DIR/etc/os_release
 
 ./build.sh --cross --arch arm64 --subset clr
 if [[ $? -ne 0 ]]; then
--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 cd $GITHUB_WORKSPACE
 
-apt install -y tree \
+apt install -y \
 	cmake llvm lld clang build-essential python-is-python3 curl git lldb \
 	libicu-dev liblttng-ust-dev libssl-dev libkrb5-dev zlib1g-dev qemu \
 	qemu-user-static binfmt-support debootstrap
