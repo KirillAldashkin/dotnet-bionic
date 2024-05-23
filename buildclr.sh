550fbe56dc006ea3093f43558dc786edf8522931
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 16:54:01 +0800

    ..

--- buildclr.sh
+++ buildclr.sh
@@ -1,5 +1,6 @@
 cd $GITHUB_WORKSPACE/runtime
 export ROOTFS_DIR=$GITHUB_WORKSPACE/runtime/.tools/android-rootfs/android-ndk-r21/sysroot
+mkdir $ROOTFS_DIR/etc
 cp $ROOTFS_DIR/android_platform $ROOTFS_DIR/etc/os_release
 
 ./build.sh --cross --arch arm64 --subset clr
