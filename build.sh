a95658a0a445b290a1c00be1c2a0c8931f88beda
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 17:14:47 +0800

    .....

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ if [[ $? -ne 0 ]]; then
 fi
 cd runtime
 # Generate the ROOTFS
-./eng/common/cross/build-android-rootfs.sh arm64 34
+./eng/common/cross/build-android-rootfs.sh
 if [[ $? -ne 0 ]]; then
     echo "Could not generate the ROOTFS"
     exit -1
