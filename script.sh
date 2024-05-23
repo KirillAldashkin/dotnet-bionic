81fd77567833f7dee6ca6cdfe5d0ca203fc4d253
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 15:17:57 +0800

    Android ROOTFS

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-cd temp
+echo "Path: $GITHUB_WORKSPACE"
 
 for pkg in cmake llvm lld clang build-essential python-is-python3 curl git lldb libicu-dev liblttng-ust-dev libssl-dev libkrb5-dev zlib1g-dev qemu qemu-user-static binfmt-support debootstrap
 do
@@ -10,8 +10,13 @@ do
 done
 
 git clone https://github.com/dotnet/runtime.git
-if [[ $? -ne 0 ]]; then
+    if [[ $? -ne 0 ]]; then
         echo "Could not clone repository"
     exit -1
 fi
 cd runtime
+
+sudo ./eng/common/cross/build-android-rootfs.sh arm64 34
+if [[ $? -ne 0 ]]; then
+    echo "Could not generate the ROOTFS"
+exit -1
