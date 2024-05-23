dbf6680efd91a1832cec0668361300f7593b64ca
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 16:15:20 +0800

    Investigate 2

--- buildclr.sh
+++ buildclr.sh
@@ -1,6 +1,6 @@
 cd runtime
 export ROOTFS_DIR=$GITHUB_WORKSPACE/runtime/.tools/android-rootfs/android-ndk-r21/sysroot
-
+tree $ROOTFS_DIR
 ./build.sh --cross --arch arm64 --subset clr
 if [[ $? -ne 0 ]]; then
     echo "Could not build the CoreCLR"
--- packages.sh
+++ packages.sh
@@ -1,14 +1,4 @@
-sudo dpkg --add-architecture arm64
-sudo bash -c 'cat > /etc/apt/sources.list.d/arm64.list <<EOF
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-backports main restricted universe multiverse
-EOF'
-sudo sed -i -e 's/deb http/deb [arch=amd64] http/g' /etc/apt/sources.list
-sudo sed -i -e 's/deb mirror/deb [arch=amd64] mirror/g' /etc/apt/sources.list
-sudo apt update
-sudo apt install -y clang llvm binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu zlib1g-dev:arm64
-
+sudo apt install -y tree
 sudo apt install -y \
 	cmake llvm lld clang build-essential python-is-python3 curl git lldb \
 	libicu-dev liblttng-ust-dev libssl-dev libkrb5-dev zlib1g-dev qemu \
