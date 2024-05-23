31692b567b26a9c500c323fdc420564cae838724
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 15:49:53 +0800

    Fix name

--- buildclr.sh
+++ buildclr.sh
@@ -1,7 +1,7 @@
 cd runtime
 export ROOTFS_DIR=$GITHUB_WORKSPACE/runtime/.tools/android-rootfs/android-ndk-r21/sysroot
 
-./build.sh --cross --arch arm64 --subset coreclr
+./build.sh --cross --arch arm64 --subset clr
 if [[ $? -ne 0 ]]; then
     echo "Could not build the CoreCLR"
     exit -1
