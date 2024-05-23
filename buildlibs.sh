1606fdfce7e3322de665f71177949774fd2cbcfb
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 15:44:42 +0800

    Fix switches

--- buildlibs.sh
+++ buildlibs.sh
@@ -1,7 +1,7 @@
 cd runtime
 export ROOTFS_DIR=$GITHUB_WORKSPACE/runtime/.tools/android-rootfs/android-ndk-r21/sysroot
 
-./build.sh --cross --arch arm64 --subsetCategory libraries
+./build.sh --cross --arch arm64 --subset libs
 if [[ $? -ne 0 ]]; then
     echo "Could not build the libraries"
     exit -1
--- packages.sh
+++ packages.sh
@@ -1,8 +1,4 @@
-for pkg in cmake llvm lld clang build-essential python-is-python3 curl git lldb libicu-dev liblttng-ust-dev libssl-dev libkrb5-dev zlib1g-dev qemu qemu-user-static binfmt-support debootstrap
-do
-    sudo apt install $pkg
-    if [[ $? -ne 0 ]]; then
-        echo "Could not install $pkg"
-        exit -1
-    fi
-done
+sudo apt install \
+	cmake llvm lld clang build-essential python-is-python3 curl git lldb \
+	libicu-dev liblttng-ust-dev libssl-dev libkrb5-dev zlib1g-dev qemu \
+	qemu-user-static binfmt-support debootstrap
