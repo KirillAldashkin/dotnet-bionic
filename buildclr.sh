6cd28fb32eddad9ced5d303ddca4105be055bc98
KirillAldashkin <a89086520092k@yandex.ru>
2024-05-23 16:33:49 +0800

    Investigating2

--- buildclr.sh
+++ buildclr.sh
@@ -1,5 +1,6 @@
 cd $GITHUB_WORKSPACE/runtime
 export ROOTFS_DIR=$GITHUB_WORKSPACE/runtime/.tools/android-rootfs/android-ndk-r21/sysroot
+tree  $ROOTFS_DIR
 
 ./build.sh --cross --arch arm64 --subset clr
 if [[ $? -ne 0 ]]; then
